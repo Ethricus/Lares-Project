2f5bce9f934af3ab7daf59079198c398e45833e8
Jack Wright <68773585+Ethirix@users.noreply.github.com>
2024-09-27 02:38:29 +0100

    Initial Camera Completed

--- Lares/Assets/Camera/Scripts/CameraController.cs
+++ Lares/Assets/Camera/Scripts/CameraController.cs
@@ -1,10 +1,11 @@
-using System;
-using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Lares.Camera.Scripts
 {
-    [ExecuteAlways, ]
+    //Determine if Camera is in correct position
+    //Raycast between Camera and center, ignore the player somehow
+    
     public class CameraController : MonoBehaviour
     {
         [Header("Objects")]
@@ -14,26 +15,64 @@ namespace Lares.Camera.Scripts
         [SerializeField] private Vector3 _cameraCenter;
         [SerializeField] private Vector2 _cameraOffset;
         [SerializeField] private float _distanceFromCenter;
-        
+
+        private PlayerControls _playerControls;
+
+        private Vector2 _inputVector;
+
+        private void Awake()
+        {
+            if (!Application.isPlaying) return;
+
+            _playerControls = new PlayerControls();
+        }
+
         private void Start()
         {
             transform.localPosition = _cameraCenter;
             _camera.transform.localPosition = new Vector3(_cameraOffset.x, _cameraOffset.y, -_distanceFromCenter);
         }
-
-        private void Update()
+        
+        private void OnEnable()
         {
-            EditorUpdate();
-            
-            //Determine if Camera is in correct position
-            //Raycast between Camera and center, ignore the player somehow
+            if (!Application.isPlaying) return;
+
+            _playerControls.Enable();
+            _playerControls.BaseControls.MoveCamera.performed += OnMoveCameraPerformed;
         }
 
-        [Conditional("UNITY_EDITOR")]
-        private void EditorUpdate()
+        private void OnDisable()
         {
+            if (!Application.isPlaying) return;
+            
+            _playerControls.Disable();
+            _playerControls.BaseControls.MoveCamera.performed -= OnMoveCameraPerformed;
+        }
+        
+        private void LateUpdate()
+        {
+            if (!Application.isPlaying) return;
+         
             transform.localPosition = _cameraCenter;
             _camera.transform.localPosition = new Vector3(_cameraOffset.x, _cameraOffset.y, -_distanceFromCenter);
+            
+            _inputVector.y = Mathf.Clamp(_inputVector.y, -90f, 90);
+            switch (_inputVector.x)
+            {
+                case >= 360f:
+                    _inputVector.x -= 360f;
+                    break;
+                case <= -360f:
+                    _inputVector.x += 360f;
+                    break;
+            }
+            
+            transform.localRotation = Quaternion.Euler(_inputVector.y, -_inputVector.x, 0);
+        }
+
+        private void OnMoveCameraPerformed(InputAction.CallbackContext context)
+        {
+            _inputVector += context.ReadValue<Vector2>();
         }
     }
 }
