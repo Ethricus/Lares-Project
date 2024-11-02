79f48247c0264f449b570f86b8aa0d21365ccacc
Erithacus <erithacusproject@gmail.com>
2024-11-02 19:29:12 +0000

    level fix

--- Lares/Assets/Player/Scripts/PlayerAnimation.cs
+++ Lares/Assets/Player/Scripts/PlayerAnimation.cs
@@ -8,17 +8,31 @@ namespace Lares.Player.Scripts
         private PlayerInput _input;
         private Animator _animator;
         private bool _isSprinting = false;
+        private Rigidbody _rigidbody;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             _input = GetComponentInParent<PlayerInput>();
             _animator = GetComponent<Animator>();
-            _input.currentActionMap.FindAction("MovePlayer").performed += MoveStart;
-            _input.currentActionMap.FindAction("MovePlayer").canceled += MoveEnd;
+            _rigidbody = GetComponentInParent<Rigidbody>();
+            //_input.currentActionMap.FindAction("MovePlayer").performed += MoveStart;
+            //_input.currentActionMap.FindAction("MovePlayer").canceled += MoveEnd;
             _input.currentActionMap.FindAction("Sprint").performed += SprintStart;
             _input.currentActionMap.FindAction("Sprint").canceled += SprintEnd;
             _input.currentActionMap.FindAction("Evade").performed += Evade;
 
+        }
+
+        private void FixedUpdate()
+        {
+            if (Mathf.Abs( Vector3.Magnitude(_rigidbody.linearVelocity)) > 0.5)
+            {
+                _animator.SetBool("Moving", true);
+            }
+            else
+            {
+                _animator.SetBool("Moving", false);
+            }
         }
 
         void MoveStart(InputAction.CallbackContext context)
